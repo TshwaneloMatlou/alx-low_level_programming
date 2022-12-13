fb029197026b3d2535e23bfce300dea3aec71793
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2022-12-13 02:19:53 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - recognize lowercase
+ *@c: First operand
+ *Return: 1 if c is uppercase and 0 if is lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+		else
+	{
+		return (0);
+	}
+}
