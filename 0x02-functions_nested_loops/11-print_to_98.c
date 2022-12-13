1cf3ced391d6881408d9fd3643f54a747e1d2da5
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2022-12-13 09:12:44 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all numbers from n o 98.
+ * @n: int variable
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	i = n;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	}
+}
