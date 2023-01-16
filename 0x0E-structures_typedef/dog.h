80a3a3a64f182da0f17dfa2a0cb5ae8fa62c3e04
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2023-01-16 04:04:10 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - Define a new struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
+ **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
