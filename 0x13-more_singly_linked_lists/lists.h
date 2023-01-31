47adaea6e1a7377d86bdbb1129e4529d1cc09eb0
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2023-01-31 10:25:04 -0800

    1. List length

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H_
 #define LISTS_H_
-
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -16,5 +17,6 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
